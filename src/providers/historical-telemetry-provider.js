a540a91a4a72b93faa852a05f36f1dcc9d9ae2fc
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2020-11-03 17:54:51 -0800

    Improve LAD and min-max request handing #3

--- src/providers/historical-telemetry-provider.js
+++ src/providers/historical-telemetry-provider.js
@@ -50,10 +50,11 @@ export default class YamcsHistoricalTelemetryProvider {
         return this.getHistory(domainObject.identifier.key,
             options.start,
             options.end,
-            options.size);
+            options.size,
+            options.strategy);
     }
 
-    getHistory(id, start, end, size=300) {
+    getHistory(id, start, end, size=300, strategy) {
         // cap size at 1000, temporarily to prevent errors
         if (size > 1000) {
             size = 1000;
@@ -61,20 +62,27 @@ export default class YamcsHistoricalTelemetryProvider {
 
         let url = this.url + 'api/archive/' + this.instance;
         url += this.getLinkParamsSpecificToId(id);
+
+        let order = 'asc';
+        let sizeParam = 'limit';
+        let convertHistory = (res) => this.convertPointHistory(id, res);
+        if (strategy && strategy.toLowerCase() === 'latest') {
+            size = 1;
+            order = 'desc'
+        } else if (strategy && strategy.toLowerCase() === 'minmax') {
+            url += '/samples';
+            sizeParam = 'count';
+            convertHistory = (res) => this.convertSampleHistory(id, res);
+        }
+
         url += '?start=' + (new Date(start).toISOString());
         url += '&stop=' + (new Date(end).toISOString());
-        url += '&limit=' + size;
-        url += "&order=asc";
+        url += `&${sizeParam}=${size}`;
+        url += `&order=${order}`;
 
         return fetch(encodeURI(url))
             .then(res => res.json())
-            .then(res => {
-                if (!(res.continuationToken)) {
-                    return this.convertPointHistory(id, res);
-                } else {
-                    return this.getSampleHistory(id, start, end, size);
-                }
-            });
+            .then(convertHistory);
     }
 
     getLinkParamsSpecificToId(id) {
@@ -83,24 +91,6 @@ export default class YamcsHistoricalTelemetryProvider {
         }
 
         return '/parameters' + idToQualifiedName(id);
-    }
-
-    getSampleHistory(id, start, end, size=300) {
-        // cap size at 1000, temporarily to prevent errors
-        if (size > 1000) {
-            size = 1000;
-        }
-
-        let url = this.url + 'api/archive/' + this.instance + '/parameters' + idToQualifiedName(id);
-        url += '/samples';
-        url += '?start=' + (new Date(start).toISOString());
-        url += '&stop=' + (new Date(end).toISOString());
-        url += '&count=' + size;
-        url += "&order=asc";
-
-        return fetch(encodeURI(url))
-            .then(res => res.json())
-            .then(res => this.convertSampleHistory(id, res));
     }
 
     convertEventHistory(id, results) {
