295202bedbee1f8da3b3710151ea06da42f0f396
Jamie Vigliotta <jamie.j.vigliotta@nasa.gov>
2021-07-27 10:27:42 -0700

    debuggin

--- src/providers/messages.js
+++ src/providers/messages.js
@@ -2,8 +2,8 @@ import * as OBJECT_TYPES from '../const';
 
 export const DATA_TYPE_EVENTS = 'events';
 export const DATA_TYPE_TELEMETRY = 'parameters';
-export const DATA_TYPE_STRING = 'string';
-export const DATA_TYPE_IMAGE = 'image';
+// export const DATA_TYPE_STRING = 'string';
+// export const DATA_TYPE_IMAGE = 'image';
 
 // YAMCS built-in types
 export const DATA_TYPE_REPLY = 'reply';
@@ -11,8 +11,8 @@ export const DATA_TYPE_REPLY = 'reply';
 const typeMap = {
     [OBJECT_TYPES.EVENTS_OBJECT_TYPE]: DATA_TYPE_EVENTS,
     [OBJECT_TYPES.TELEMETRY_OBJECT_TYPE]: DATA_TYPE_TELEMETRY,
-    [OBJECT_TYPES.STRING_OBJECT_TYPE]: DATA_TYPE_STRING,
-    [OBJECT_TYPES.IMAGE_OBJECT_TYPE]: DATA_TYPE_IMAGE
+    [OBJECT_TYPES.STRING_OBJECT_TYPE]: DATA_TYPE_TELEMETRY,
+    [OBJECT_TYPES.IMAGE_OBJECT_TYPE]: DATA_TYPE_TELEMETRY
 };
 
 export const SUBSCRIBE = buildSubscribeMessages();
