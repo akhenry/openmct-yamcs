8561298744089a2f0d20cd13c939fcb62a7b3f42
Jamie Vigliotta <jamie.j.vigliotta@nasa.gov>
2021-10-04 14:13:05 -0700

    debugging

--- src/providers/object-provider.js
+++ src/providers/object-provider.js
@@ -341,7 +341,7 @@ export default class YamcsObjectProvider {
             if (aggregateHasMembers) {
                 let memberMetadata = this.formatAggregateMembers(parameter.type.member, parameter.name);
                 if (['pose', 'pose_orientation', 'pose_position'].includes(name)) {
-                    console.log('parameter', name, parameter);
+                    console.log('parameter', name, memberMetadata);
                 }
                 obj.telemetry.values = obj.telemetry.values.concat(memberMetadata);
             }
