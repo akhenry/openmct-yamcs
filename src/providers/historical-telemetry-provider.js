1060d42ebe43bf346dac0f6a8068cb288ade4ba4
Scott Bell <scott@traclabs.com>
2022-10-21 11:55:40 +0200

    rename function

--- src/providers/historical-telemetry-provider.js
+++ src/providers/historical-telemetry-provider.js
@@ -54,7 +54,7 @@ export default class YamcsHistoricalTelemetryProvider {
     request(domainObject, options) {
         options = { ...options };
         this.standardizeOptions(options, domainObject);
-        if ((options.strategy === 'latest') && options.timeContext?.isRealtime()) {
+        if ((options.strategy === 'latest') && options.timeContext?.isRealTime()) {
             // Latest requested in realtime, use cached websocket data
             return [];
         }
