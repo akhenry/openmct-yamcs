61de905a52ca4fb03734414a81e7cacaf912ddc0
Khalid Adil <khalidadil29@gmail.com>
2023-08-01 14:08:44 -0500

    More logs

--- src/utils.js
+++ src/utils.js
@@ -186,6 +186,7 @@ async function accumulateResults(url, options, property, soFar, totalLimit, toke
 
     const fetchResult = await fetch(newUrl, options);
     const result = await fetchResult.json();
+    console.log(result);
 
     if (property in result) {
         soFar = soFar.concat(result[property]);
