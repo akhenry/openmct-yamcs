576be81b4bcd21a16c6d4dc04106ec07170e3e1c
John Hill <john.c.hill@nasa.gov>
2023-08-09 11:26:49 -0700

    Update git-opensource-tests.sh

--- tests/git-opensource-tests.sh
+++ tests/git-opensource-tests.sh
@@ -4,24 +4,21 @@
 # repository.
 #
 # Usage:
-# ./git-opensource-tests.sh [branch-name]
+# ./tests/git-opensource-tests.sh [branch-name]
 #
 # If no branch name is provided, the script defaults to the 'master' branch.
 
 set -e # Exit immediately if a command exits with a nonzero exit value
 
-SCRIPT_PATH=${0%/*} # Get the relative path to the script dir from the cwd
-if [ "$0" != "$SCRIPT_PATH" ] && [ "$SCRIPT_PATH" != "" ]; then 
-    cd "$SCRIPT_PATH"
-fi
+SCRIPT_DIR="$(dirname "$0")" # Get the directory of the script
+REPO_DIR="$SCRIPT_DIR/.."   # Get the root directory of the repository
 
-rm -rf e2e/opensource
+rm -rf "$REPO_DIR/e2e/opensource"
 
 REPO_URL=https://github.com/nasa/openmct.git
 REPO_BRANCH=${1:-master} # Use the first argument as the branch, or default to 'master'
 REPO_PATH=e2e
-LOCAL_REPO_ROOT="e2e/opensource"
-E2E_PATH="e2e/opensource/e2e"
+LOCAL_REPO_ROOT="$REPO_DIR/e2e/opensource"
 
 git clone --no-checkout --depth 1 --branch $REPO_BRANCH $REPO_URL "$LOCAL_REPO_ROOT"
 cd "$LOCAL_REPO_ROOT"
@@ -30,12 +27,12 @@ echo "/$REPO_PATH/**" > .git/info/sparse-checkout
 git read-tree -m -u HEAD
 
 # Debugging information
-echo "Listing contents of $E2E_PATH:"
-ls -l $E2E_PATH
+echo "Listing contents of $LOCAL_REPO_ROOT:"
+ls -l
 
 # Move all required files and folders
-mv $E2E_PATH/*Fixtures.js $E2E_PATH/appActions.js $E2E_PATH/* $E2E_PATH/.eslintrc.js ../opensource
+mv "$LOCAL_REPO_ROOT/e2e"/*Fixtures.js "$LOCAL_REPO_ROOT/e2e/appActions.js" "$LOCAL_REPO_ROOT/e2e"/* "$LOCAL_REPO_ROOT/e2e/.eslintrc.js" "$REPO_DIR/opensource"
 
 # Cleanup
-rm -rf $E2E_PATH
+rm -rf "$LOCAL_REPO_ROOT/e2e"
 
