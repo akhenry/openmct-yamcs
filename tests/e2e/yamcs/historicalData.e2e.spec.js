2e90549d5a4cf9bc013021a7070b554ef5927f92
Shefali <simplyrender@gmail.com>
2023-08-22 10:37:17 -0700

    Add test to ensure that enum paramter types also dont use the samples endpoint in the table view

--- tests/e2e/yamcs/historicalData.e2e.spec.js
+++ tests/e2e/yamcs/historicalData.e2e.spec.js
@@ -120,7 +120,51 @@ test.describe("Samples endpoint with useRawValue search param @yamcs", () => {
         await secondMyProjectTriangle.click();
 
         await page.waitForLoadState('networkidle');
-        await page.locator('text=CCSDS_Packet_Length').first().click();
+        await page.locator('text=Enum_Para_1').first().click();
+        await page.waitForLoadState('networkidle');
+
+        //switch to table view
+        networkRequests = [];
+        await page.locator("button[title='Change the current view']").click();
+        await page.getByRole('menuitem', { name: /Telemetry Table/ }).click();
+        await page.waitForLoadState('networkidle');
+
+        // wait for debounced requests in YAMCS Latest Telemetry Provider to finish
+        await new Promise(resolve => setTimeout(resolve, 500));
+
+        filteredRequests = filterNonFetchRequests(networkRequests);
+
+        // 1.  batched request for latest telemetry using the bulk API
+        // 2.  samples request for telemetry data
+        //Switch view to table
+        // 3.  batched request for latest telemetry using the bulk API
+        // 4.  parameters history request for telemetry data
+        // 5.  parameters history request for telemetry data with token
+        const samplesRequests = filteredRequests.filter(request => request.url().indexOf('/samples') > -1);
+        const nonSampleRequests = filteredRequests.filter(request => request.url().indexOf('/samples') < 0);
+        expect(samplesRequests.length).toBe(0);
+        expect(nonSampleRequests.length).toBe(filteredRequests.length);
+    });
+
+    test('When in table view, samples endpoint and useRawValue are not used for enum type parameters', async ({ page }) => {
+        networkRequests = [];
+        filteredRequests = [];
+        page.on('request', (request) => networkRequests.push(request));
+        // Go to baseURL
+        await page.goto("./", { waitUntil: "networkidle" });
+
+        // Change to fixed time
+        await setFixedTimeMode(page);
+
+        const myProjectTreeItem = page.locator('.c-tree__item').filter({ hasText: 'myproject'});
+        await expect(myProjectTreeItem).toBeVisible();
+        const firstMyProjectTriangle = myProjectTreeItem.first().locator('span.c-disclosure-triangle');
+        await firstMyProjectTriangle.click();
+        const secondMyProjectTriangle = myProjectTreeItem.nth(1).locator('span.c-disclosure-triangle');
+        await secondMyProjectTriangle.click();
+
+        await page.waitForLoadState('networkidle');
+        await page.locator('text=Enum_Para_1').first().click();
         await page.waitForLoadState('networkidle');
 
         //switch to table view
