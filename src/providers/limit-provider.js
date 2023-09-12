43eb06990193ee6a613de0b4de5ccb16cb3f717f
Mazzella, Jesse D. (ARC-TI)[KBR Wyle Services, LLC] <jesse.d.mazzella@nasa.gov>
2023-09-12 14:02:13 -0700

    refactor: use `async`

--- src/providers/limit-provider.js
+++ src/providers/limit-provider.js
@@ -100,7 +100,7 @@ export default class LimitProvider {
         const limits = domainObject.configuration.limits;
 
         return {
-            limits: () => Promise.resolve(limits)
+            limits: async () => limits
         };
     }
 
