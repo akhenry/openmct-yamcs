afe59f21a47277aa4fd790d9ace990f03fd55595
Scott Bell <scott@scottbell.name>
2023-11-02 11:13:36 +0100

    wip

--- tests/k6/websocket-subscriptions.js
+++ tests/k6/websocket-subscriptions.js
@@ -1,4 +1,5 @@
-import { randomString, randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+/* global __VU */
+
 import ws from 'k6/ws';
 import { check } from 'k6';
 
@@ -7,20 +8,53 @@ export const options = {
     iterations: 10
 };
 
-export default function () {
-    // URL should be YAMCS server's websocket endpoint
-    const url = `ws://localhost:8090/ws`;
+export default () => {
+    console.info(`Starting YAMCS websocket load test for ${__VU}`);
+    const wsURL = `ws://localhost:8090/ws`;
     const params = { tags: { my_tag: 'YAMCS load testing connection' } };
+    let counter = 0;
 
-    const res = ws.connect(url, params, function (socket) {
-        socket.on('open', function open() {
-            console.info(`Connected to YAMCS`);
+    const res = ws.connect(wsURL, params, function (socket) {
+        socket.on('open', () => {
+            console.log(`VU ${__VU}: connected`);
+            console.info(`🔌 Established websocket connection to ${wsURL}`);
         });
 
-        socket.on('close', function () {
-            console.info(`Diconnected from YAMCS`);
+        socket.on('message', (rawMessage) => {
+            const incomingMessage = JSON.parse(rawMessage);
+            console.info(`📨 Received message:`, incomingMessage);
+            counter += 1;
+            if (counter === 10000) {
+                console.info(`💸 Received ${counter} messages. Closing WebSocket.`);
+                socket.close();
+            }
         });
+
+        socket.on('error', (error) => {
+            console.error(`🚨 Websocket error, closing websocket`, error);
+            socket.close();
+        });
+
+        socket.on('close', () => {
+            console.warn('🚪 Websocket closed.');
+        });
+
+        // send subscription request message
+        socket.send(JSON.stringify({
+            type: 'float',
+            path: '/myproject/Gyro.x',
+            id: 'YAMCS load testing connection',
+            options: {
+                instance: 'YAMCS load testing connection',
+                processor: 'realtime',
+                id: [{
+                    name: 'YAMCS load testing connection'
+                }],
+                sendFromCache: true,
+                updateOnExpiration: true
+            }
+        }));
     });
 
-    check(res, { 'Connected successfully': (r) => r && r.status === 101 });
-}
+    check(res, { 'Connected successfully': (r) => r && r.status === 200 });
+};
