4ba25c943acc67795b0793e0464f2b0e44f0e852
Scott Bell <scott@traclabs.com>
2024-01-12 16:19:48 +0100

    general solution for memory eviction

--- .webpack/webpack.dev.js
+++ .webpack/webpack.dev.js
@@ -30,34 +30,4 @@ export default merge(common, {
     context: projectRootDir,
     mode: 'development',
     devtool: 'eval-source-map',
-    entry: {
-        'openmct-yamcs-example': path.resolve(projectRootDir, 'example/index.js')
-    },
-    devServer: {
-        compress: true,
-        port: 9000,
-        static: [{
-            // eslint-disable-next-line no-undef
-            directory: path.join(projectRootDir, 'example')
-        }, {
-            // eslint-disable-next-line no-undef
-            directory: path.join(projectRootDir, '/node_modules/openmct/dist'),
-            publicPath: '/node_modules/openmct/dist'
-        }],
-        proxy: {
-            "/yamcs-proxy/*": {
-                target: "http://0.0.0.0:8090/",
-                secure: false,
-                changeOrigin: true,
-                pathRewrite: { '^/yamcs-proxy/': '' }
-            },
-            "/yamcs-proxy-ws/*": {
-                target: "ws://0.0.0.0:8090/api/websocket",
-                secure: false,
-                changeOrigin: true,
-                ws: true,
-                pathRewrite: { '^/yamcs-proxy-ws/': '' }
-            }
-        }
-    }
 });
--- tests/e2e/playwright-quickstart.config.js
+++ tests/e2e/playwright-quickstart.config.js
@@ -3,9 +3,10 @@
 
 /** @type {import('@playwright/test').PlaywrightTestConfig<{ failOnConsoleError: boolean, myItemsFolderName: string }>} */
 const config = {
-    retries: 1,
+    retries: 0,
     testDir: '.',
     testMatch: '**/*.e2e.spec.js',
+    testIgnore: '**/*.perf.spec.js',
     timeout: 30 * 1000,
     use: {
         headless: false,
