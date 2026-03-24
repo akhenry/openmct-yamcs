40404cb300e07948b026ec2ba98130d193dacac4
David Tsay <david.e.tsay@gmail.com>
2026-03-23 17:03:32 -0700

    now is always the global now

--- src/providers/historical-telemetry-provider.js
+++ src/providers/historical-telemetry-provider.js
@@ -59,10 +59,8 @@ export default class YamcsHistoricalTelemetryProvider {
     async request(domainObject, options) {
         options = { ...options };
         this.standardizeOptions(options, domainObject);
-        const timeContext = options.timeContext ?? this.openmct.time;
-
         const supportsLatest = options.strategy === 'latest' && !isEventType(domainObject.type);
-        const isNotPast = options.end >= timeContext.now();
+        const isNotPast = options.end >= this.openmct.time.now();
 
         if (supportsLatest && isNotPast) {
             const mctDatum = await this.latestTelemetryProvider.requestLatest(domainObject);
