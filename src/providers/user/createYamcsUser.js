43d678d122e377b48ef351a94d533ea1d08212c7
David Tsay <david.e.tsay@gmail.com>
2026-05-14 09:32:40 -0700

    update for yamcs api changes (cherry picked from commit 91b9478b023411f3e78981fe9d3ca964bd785a43)

--- src/providers/user/createYamcsUser.js
+++ src/providers/user/createYamcsUser.js
@@ -51,7 +51,7 @@ export default function createYamcsUser(UserClass) {
             confirmationTime,
             lastLoginTime,
             roles = [],
-            objectPrivilege
+            objectPrivileges
         }) {
             super(name, name); // id, name (yamcs doesn't provide an id)
 
@@ -61,7 +61,7 @@ export default function createYamcsUser(UserClass) {
             this.confirmationTime = confirmationTime;
             this.lastLoginTime = lastLoginTime;
             this.roles = roles.map(role => role.name);
-            this.objectPrivileges = objectPrivilege;
+            this.objectPrivileges = objectPrivileges;
         }
 
         getWriteParameters() {
@@ -69,7 +69,7 @@ export default function createYamcsUser(UserClass) {
                 return [];
             }
 
-            const writeParameters = this.objectPrivileges.find(entry => entry.type === 'WriteParameter')?.object || [];
+            const writeParameters = this.objectPrivileges.find(entry => entry.type === 'WriteParameter')?.objects || [];
 
             return writeParameters;
         }
